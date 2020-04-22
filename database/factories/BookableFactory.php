71d35050ef2f7bbf26ebcb2925f9cdf13085791f
Fadi Serapian <fadi.serapian@gmail.com>
2020-04-21 23:54:28 -0400

    create bookables table and seed it with fake data

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Bookable;
+use Illuminate\Support\Arr;
+use Faker\Generator as Faker;
+
+$bookableNames = [
+    'Villa',
+    'House',
+    'Cuttage',
+    'Hutt',
+    'Mansion',
+    'Stadium',
+    'Hall'
+];
+
+$factory->define(Bookable::class, function (Faker $faker) use ($bookableNames) {
+    return [
+        'title' => $faker->city . ' ' . Arr::random($bookableNames),
+        'description' => $faker->sentence(),
+    ];
+});
