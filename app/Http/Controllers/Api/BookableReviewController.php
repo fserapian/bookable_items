f1bdb3e137687c54305c07e4a61bfa5780671afe
Fadi Serapian <fadi.serapian@gmail.com>
2020-04-25 18:52:02 -0400

    add BookableReviewController

---
+++ app/Http/Controllers/Api/BookableReviewController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Bookable;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BookableReviewController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, $id)
+    {
+        $bookable = Bookable::findOrFail($id);
+        return $bookable->reviews()->latest()->get();
+    }
+}
